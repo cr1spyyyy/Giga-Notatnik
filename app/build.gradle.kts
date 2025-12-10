9aa21455b45e3b0b80b5afeffbe0979bb97fe822
Michał Kalisiak <kalisiak.m1ch4l@gmail.com>
2025-12-10 19:39:29 +0100

    fixed version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.example.giganotatnik"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
